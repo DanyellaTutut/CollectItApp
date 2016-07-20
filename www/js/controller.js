211e3a66e2d081ffa3a1c08f10b2aedb19b5e13b
DanyellaTutut <danyellaaah@gmail.com>
2016-07-20 12:15:11 +0800

    Calendar changes

---
+++ www/js/controller.js
@@ -0,0 +1,16 @@
+(function(){
+	
+	var app = angular.module('mlmsApp', [ ]);
+
+	app.controller('myController', function (){
+	this.emp = gem;
+	});
+
+	var gem = {
+		firstName : 'Daniella',
+		signIn : true
+	}
+
+	
+
+})();
